1c834295a86baf5a3b9263ed2ae3c62c577292fb
HaloC23 <69718744+HaloC23@users.noreply.github.com>
2020-12-05 19:28:02 +0000

    Initial commit

---
+++ src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1> Travelling </h1>
+      <p> Where I would like to go... </p>
+      <ul>
+        <li>Japan</li>
+        <li>Canada</li>
+        <li>Thailand</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import MyInfo from "./MyInfo.js";
+
+ReactDom.render(<MyInfo />, document.getElementById("root"));
