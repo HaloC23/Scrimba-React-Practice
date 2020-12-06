df646612885ed5be0b65137d686ba0e71c83e213
HC23 <69718744+HaloC23@users.noreply.github.com>
2020-12-06 18:58:32 +0000

    Created a component Created a footer component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-
+import Footer from "./Footer";
 function App() {
   return (
     <div>
-      <h1> In the new year </h1>
-      <p> Where I would like to go... </p>
-      <ul>
-        <li>Japan</li>
-        <li>Canada</li>
-        <li>Thailand</li>
-      </ul>
+      <nav>
+        <h1> In the new year </h1>
+        <p> Where I would like to go... </p>
+        <ul>
+          <li>Japan</li>
+          <li>Canada</li>
+          <li>Thailand</li>
+        </ul>
+      </nav>
+      <main>
+        <p>
+          Some rather obsure sample text or speech about some nonsensical
+          nonsene.
+        </p>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <h4>
+          {" "}
+          Coded by this one{" "}
+          <a href="https://github.com/HaloC23"> over here! </a>{" "}
+        </h4>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
