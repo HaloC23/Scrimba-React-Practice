066b0225244d79c34220ddb3d4084aed415e0dd0
HC23 <69718744+HaloC23@users.noreply.github.com>
2021-01-01 14:28:48 +0000

    Components & Styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,11 +4,12 @@ function Footer() {
   return (
     <div>
       <footer>
-        <h4>
+        <h4 className="CodedBy">
           {" "}
           Coded by this one{" "}
           <a href="https://github.com/HaloC23"> over here! </a>{" "}
         </h4>
+        <h6 className="Opin"> This is a work of opinion. </h6>
       </footer>
     </div>
   );
